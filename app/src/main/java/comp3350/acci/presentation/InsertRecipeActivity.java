0cbaf676f57d613b63c4762b47f33c89a270278e
Ackeso <caelan.myskiw@gmail.com>
2023-02-11 16:10:50 -0600

    Turned the insert recipe page into functionally adding a recipe to the persistence

--- app/src/main/java/comp3350/acci/presentation/InsertRecipeActivity.java
+++ app/src/main/java/comp3350/acci/presentation/InsertRecipeActivity.java
@@ -7,15 +7,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import comp3350.acci.R;
+import comp3350.acci.business.RecipeManager;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link InsertRecipeActivity#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class InsertRecipeActivity extends Fragment {
+public class InsertRecipeActivity extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -55,12 +58,45 @@ public class InsertRecipeActivity extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_insert_recipe, container, false);
+
+        // Inflate the view for this fragment (ie. show it on the screen)
+        View view =  inflater.inflate(R.layout.activity_insert_recipe, container, false);
+
+        //make sure this class knows it's the on listening for this button to be pressed
+        Button addRecipe = (Button) view.findViewById(R.id.insert_recipe_btnadd);
+        addRecipe.setOnClickListener(this);
+
+        return view;
+    }
+
+
+    //add the recipe to the persistence (with a manager)
+    @Override
+    public void onClick(View view) {
+        Button addRecipeButton = (Button) getView().findViewById(R.id.insert_recipe_btnadd);
+
+        //Get user inputted text fields
+        EditText recipeText = (EditText) getView().findViewById(R.id.insert_recipe_txtname);
+        EditText authorText = (EditText) getView().findViewById(R.id.insert_recipe_txtauthor);
+        EditText difficultyText = (EditText) getView().findViewById(R.id.insert_recipe_txtdifficulty);
+
+
+        //convert text fields to strings
+        String recipeName = recipeText.getText().toString();
+        String authorName = authorText.getText().toString();
+        String difficulty = difficultyText.getText().toString();
+        //create a manager
+        RecipeManager manager = new RecipeManager();
+
+
+        manager.createRecipeFrame(null, recipeName,  difficulty);
+
+        addRecipeButton.setText("Recipe Added!");
     }
 }
