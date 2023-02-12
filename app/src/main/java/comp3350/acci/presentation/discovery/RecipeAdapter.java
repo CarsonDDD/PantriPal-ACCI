55496d730def2d87d8f570a74e6611c5691db2a5
offenloa <ayoffenloch@gmail.com>
2023-02-12 12:16:48 -0600

    add instructions back in

--- app/src/main/java/comp3350/acci/presentation/discovery/RecipeAdapter.java
+++ app/src/main/java/comp3350/acci/presentation/discovery/RecipeAdapter.java
@@ -38,7 +38,7 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeCardViewHolder>{
         holder.textView_title.setText(list.get(position).getName());
         //holder.textView_favorites.setText("3");
         holder.textView_author.setText(list.get(position).getAuthor().getUserName());
-        //holder.textView_instructions.setText(list.get(position).getInstructions());
+        holder.textView_instructions.setText(list.get(position).getInstructions());
         holder.textView_difficulty.setText(list.get(position).getDifficulty());
         //holder.imageView_food.setImageBitmap();
     }
@@ -53,7 +53,7 @@ class RecipeCardViewHolder extends RecyclerView.ViewHolder{
 
     CardView recipe_container;
     TextView textView_title;
-    //TextView textView_instructions;
+    TextView textView_instructions;
     TextView textView_difficulty;
     ImageView imageView_food;
     //TextView textView_favorites;
@@ -67,8 +67,8 @@ class RecipeCardViewHolder extends RecyclerView.ViewHolder{
         imageView_food = itemView.findViewById(R.id.imageView_food);
         textView_author = itemView.findViewById(R.id.textView_author);
         //textView_favorites = itemView.findViewById(R.id.textView_favorites);
-        //textView_instructions = itemView.findViewById(R.id.textView_instructions);
+        textView_instructions = itemView.findViewById(R.id.textView_instructions);
         textView_difficulty = itemView.findViewById(R.id.textView_difficulty);
-        //textView_instructions.setMovementMethod(new ScrollingMovementMethod());
+        textView_instructions.setMovementMethod(new ScrollingMovementMethod());
     }
 }
