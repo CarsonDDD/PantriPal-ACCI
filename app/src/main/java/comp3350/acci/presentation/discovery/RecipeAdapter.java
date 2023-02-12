23c3d4e497ce11a7d970d7bcfefae8d06230c698
Carson <carson-dickinson@hotmail.com>
2023-02-12 12:22:34 -0600

    Readd instructions to recipe card

--- app/src/main/java/comp3350/acci/presentation/discovery/RecipeAdapter.java
+++ app/src/main/java/comp3350/acci/presentation/discovery/RecipeAdapter.java
@@ -19,6 +19,9 @@ import comp3350.acci.objects.Recipe;
 
 public class RecipeAdapter extends RecyclerView.Adapter<RecipeCardViewHolder>{
 
+    final int INSTRUCTION_CUTOFF = 50;
+    final int CUTTOFF_SIZE = 90;
+
     Context context;
     List<Recipe> list;
 
@@ -38,7 +41,12 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeCardViewHolder>{
         holder.textView_title.setText(list.get(position).getName());
         //holder.textView_favorites.setText("3");
         holder.textView_author.setText(list.get(position).getAuthor().getUserName());
-        //holder.textView_instructions.setText(list.get(position).getInstructions());
+
+        String instructions = list.get(position).getInstructions();
+        if(instructions.length() > INSTRUCTION_CUTOFF){
+            holder.textView_instructions.setHeight(CUTTOFF_SIZE);
+        }
+        holder.textView_instructions.setText(list.get(position).getInstructions());
         holder.textView_difficulty.setText(list.get(position).getDifficulty());
         //holder.imageView_food.setImageBitmap();
     }
@@ -53,7 +61,7 @@ class RecipeCardViewHolder extends RecyclerView.ViewHolder{
 
     CardView recipe_container;
     TextView textView_title;
-    //TextView textView_instructions;
+    TextView textView_instructions;
     TextView textView_difficulty;
     ImageView imageView_food;
     //TextView textView_favorites;
@@ -67,8 +75,8 @@ class RecipeCardViewHolder extends RecyclerView.ViewHolder{
         imageView_food = itemView.findViewById(R.id.imageView_food);
         textView_author = itemView.findViewById(R.id.textView_author);
         //textView_favorites = itemView.findViewById(R.id.textView_favorites);
-        //textView_instructions = itemView.findViewById(R.id.textView_instructions);
+        textView_instructions = itemView.findViewById(R.id.textView_instructions);
         textView_difficulty = itemView.findViewById(R.id.textView_difficulty);
-        //textView_instructions.setMovementMethod(new ScrollingMovementMethod());
+        textView_instructions.setMovementMethod(new ScrollingMovementMethod());
     }
 }
