70c1fae61b4baf09154aca92a652088083646e71
Carson <carson-dickinson@hotmail.com>
2023-02-16 23:47:43 -0600

    Add hide/show to nav bar

--- app/src/main/java/comp3350/acci/presentation/MainActivity.java
+++ app/src/main/java/comp3350/acci/presentation/MainActivity.java
@@ -3,10 +3,13 @@ package comp3350.acci.presentation;
 import android.app.ActionBar;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentContainerView;
 import androidx.fragment.app.FragmentManager;
@@ -61,12 +64,29 @@ public class MainActivity extends AppCompatActivity {
 
     public void hideNavigationBar(){
         BottomNavigationView bar = binding.navigationBar;
-        bar.setVisibility(View.INVISIBLE);
-
         FragmentContainerView fragment = binding.currentFragment;
+        ConstraintLayout layout = binding.container;
+
+        bar.getLayoutParams().height = 0;
         //fragment.
 
+        ConstraintSet constraintSet = new ConstraintSet();
+        constraintSet.clone(layout);
+        constraintSet.connect(fragment.getId(),ConstraintSet.BOTTOM,layout.getId(),ConstraintSet.BOTTOM,0);
+        constraintSet.applyTo(layout);
+    }
 
+    public void showNavigationBar(){
+        BottomNavigationView bar = binding.navigationBar;
+        FragmentContainerView fragment = binding.currentFragment;
+        ConstraintLayout layout = binding.container;
+
+        bar.getLayoutParams().height = ViewGroup.LayoutParams.WRAP_CONTENT; // Maybe?
+
+        ConstraintSet constraintSet = new ConstraintSet();
+        constraintSet.clone(layout);
+        constraintSet.connect(fragment.getId(),ConstraintSet.BOTTOM, bar.getId(),ConstraintSet.TOP,0);
+        constraintSet.applyTo(layout);
     }
 
 
