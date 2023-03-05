4eb617ae6099fb3340df72c0603adbf460b327b3
Caelan Myskiw <caelan.myskiw@gmail.com>
2023-03-05 17:39:18 -0600

    Made RecipeView activity actually show the recipe that was clicked on

--- app/src/main/java/comp3350/acci/presentation/fragments/RecipeViewActivity.java
+++ app/src/main/java/comp3350/acci/presentation/fragments/RecipeViewActivity.java
@@ -1,12 +1,16 @@
 package comp3350.acci.presentation.fragments;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import org.w3c.dom.Text;
 
 import comp3350.acci.R;
 import comp3350.acci.objects.Recipe;
@@ -29,13 +33,29 @@ public class RecipeViewActivity extends ACCIFragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.activity_recipeview, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
 
+
+        //Get the required textviews from the layout
+        TextView titleView = (TextView) getView().findViewById(R.id.recipe_title);
+        TextView authorView = (TextView) getView().findViewById(R.id.recipe_author);
+        TextView instructionView = (TextView) getView().findViewById(R.id.recipe_instructions);
+        TextView difficultyView = (TextView) getView().findViewById(R.id.recipe_difficulty);
+
+        //set the textviews to the values from the recipe:
+        titleView.setText(recipe.getName());
+        authorView.setText("By " + recipe.getAuthor().getUserName());
+        difficultyView.setText("Difficulty: " + recipe.getDifficulty());
+        instructionView.setText("Instructions:\n" + recipe.getInstructions());
+
     }
-
-
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
 }
