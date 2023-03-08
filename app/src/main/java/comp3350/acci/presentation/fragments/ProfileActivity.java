31ef8e2af316e912039808e6b682cf0312c4f44e
Carson <carson-dickinson@hotmail.com>
2023-03-07 18:51:43 -0600

    Add profile page

--- app/src/main/java/comp3350/acci/presentation/fragments/ProfileActivity.java
+++ app/src/main/java/comp3350/acci/presentation/fragments/ProfileActivity.java
@@ -6,12 +6,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.List;
+
 import comp3350.acci.R;
+import comp3350.acci.business.implementation.RecipeCreator;
+import comp3350.acci.business.interfaces.RecipeManager;
+import comp3350.acci.business.listeners.RecipeClickListener;
+import comp3350.acci.objects.Recipe;
 import comp3350.acci.presentation.MainActivity;
+import comp3350.acci.presentation.fragments.discovery.RecipeAdapter;
 
 public class ProfileActivity extends ACCIFragment {
 
+    private RecyclerView savedRecipesView;
+    private RecyclerView userRecipesView;
 
+    // This should also take a user as a parameter
     public ProfileActivity(MainActivity mainActivity) {
         super(mainActivity);
         this.hasNavigationBar = true;
@@ -25,4 +43,68 @@ public class ProfileActivity extends ACCIFragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.activity_userprofile, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+        TabLayout tabLayout = view.findViewById(R.id.tab_layout);
+        savedRecipesView = view.findViewById(R.id.saved_recipes_recycler);
+        userRecipesView = view.findViewById(R.id.user_recipes_recycler);
+
+        // set up TabLayout
+        tabLayout.addTab(tabLayout.newTab().setText("Saved Recipes"));
+        tabLayout.getTabAt(0).setCustomView(savedRecipesView);
+
+        tabLayout.addTab(tabLayout.newTab().setText("User Recipes"));// Possible change this to <username>
+        tabLayout.getTabAt(1).setCustomView(userRecipesView);
+
+        // set up RecyclerViews+
+        RecipeManager rm = new RecipeCreator();
+        List<Recipe> recipeList = rm.getRecipes();
+
+        List<Recipe> userRecipes = recipeList;
+        List<Recipe> savedRecipes = recipeList;
+        savedRecipesView.setAdapter(new RecipeAdapter(this.getContext(), savedRecipes, recipeClickListener));
+        userRecipesView.setAdapter(new RecipeAdapter(this.getContext(), userRecipes, recipeClickListener));
+
+        // hide/show correct recipe for given tab? Do I need this? Does this get handled
+        // by the tab layout itself? as in no overlapping views?
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch (tab.getPosition()) {
+                    case 0:
+                        savedRecipesView.setVisibility(View.VISIBLE);
+                        userRecipesView.setVisibility(View.GONE);
+                        break;
+                    case 1:
+                        savedRecipesView.setVisibility(View.GONE);
+                        userRecipesView.setVisibility(View.VISIBLE);
+                        break;
+                }
+            }
+
+            // interface leftovers
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {}
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {}
+        });
+    }
+
+    // This function is copypaste from DiscoveryActivity, as clicking on the card should have the same
+    // effect. I dont know to make it properly/automatic where I dont need to copy paste this function.
+    // One option would be to move this listener into the RecipeAdapter itself, however, that means we would
+    // need to pass a reference to the AppCompact (since we change the display on click). Passing it in
+    // feels too dirty (even for me ;] ) and probably breaks something mentioned in the lectures; for now it stays here.
+    // TODO: make this function not need to be hardcoded into the classes which use a RecipeAdapter. All RecipeAdapters
+    //  should(?) have the same functionality when clicking on a recipe card.
+    private RecipeClickListener recipeClickListener = new RecipeClickListener() {
+        @Override
+        public void onRecipeClick(Recipe recipe) {
+            getAppCompact().changeFragment(new RecipeViewActivity(getAppCompact(), recipe));
+        }
+    };
 }
