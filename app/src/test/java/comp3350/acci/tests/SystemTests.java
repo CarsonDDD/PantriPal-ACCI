45ec8fd4985ecc4e28015ca0136509e18ff97589
Caelan Myskiw <caelan.myskiw@gmail.com>
2023-04-05 18:13:47 -0500

    Added dependencies and framework for system tests

---
+++ app/src/test/java/comp3350/acci/tests/SystemTests.java
@@ -0,0 +1,4 @@
+package comp3350.acci.tests;
+
+public class SystemTests {
+}
---
+++ app/src/test/java/comp3350/acci/tests/presentation/DiscoveryTest.java
@@ -0,0 +1,36 @@
+package comp3350.acci.tests.presentation;
+
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+
+import comp3350.acci.R;
+import comp3350.acci.presentation.MainActivity;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class DiscoveryTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void discoverRecipes() {
+//        onView(withId(R.id.button))
+    }
+}
